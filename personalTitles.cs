b52819a440f9a80cfc376cd2d232c0f70c1f2dbf
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 19:29:17 +0200

    Create personalTitles.cs

---
+++ personalTitles.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace nestedCondStateLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string sex = Console.ReadLine();
+
+            if (sex == "f")
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+        }
+    }
+}
