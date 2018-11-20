1a0cc82607e6caa8ad9f095715e7324b6b5ecc95
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 19:32:59 +0200

    Create invalidNumber.cs

---
+++ invalidNumber.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace nestedCondStateLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 100 && num <= 200)
+            {
+               
+            }
+            else if (num != 0)
+            {
+                Console.WriteLine("invalid");
+            }
+          
+
+        }
+    }
+}
